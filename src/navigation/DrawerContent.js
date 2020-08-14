656b56c34b7ef4f655579847b0b53994bac17008
Inthu <thanasingam001@gmail.com>
2020-08-15 00:03:30 +0530

    reduced repeated drawer

---
+++ src/navigation/DrawerContent.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { View, Text } from "react-native";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from "@react-navigation/drawer";
+import { Avatar, Divider, Caption } from "react-native-paper";
+import { useSelector } from "react-redux";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import Colors from "../constants/Colors";
+
+const DrawerContent = ({ children }) => {
+  const firstName = useSelector((state) => state.auth.firstName);
+  const lastName = useSelector((state) => state.auth.lastName);
+
+  const Drawer = createDrawerNavigator();
+
+  return (
+    <Drawer.Navigator
+      drawerContentOptions={{ activeTintColor: Colors.primaryOrange }}
+      initialRouteName="HomeDrawer"
+      drawerContent={(props) => (
+        <DrawerContentScrollView contentContainerStyle={{ flex: 1 }}>
+          <View style={{ flex: 1 }}>
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <Avatar.Image
+                source={{
+                  uri:
+                    "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+                }}
+                style={{ margin: 15 }}
+                size={80}
+              />
+
+              <View>
+                <Text style={{ fontSize: 15, fontWeight: "bold" }}>
+                  {`${firstName} ${lastName}`}
+                </Text>
+                <View style={{ flexDirection: "row" }}>
+                  <Caption style={{ lineHeight: 14, marginRight: 3 }}>
+                    5.0
+                  </Caption>
+                  <MaterialCommunityIcons
+                    name="star"
+                    style={{ alignSelf: "center" }}
+                  />
+                </View>
+              </View>
+            </View>
+            <Divider />
+
+            <View style={{ flex: 1, marginTop: 10 }}>
+              <DrawerItemList {...props} />
+              <Divider />
+            </View>
+            <View style={{ marginVertical: 20, justifyContent: "center" }}>
+              <Divider />
+              <DrawerItem
+                label="Log Out"
+                //onPress={logoutHandler}
+                icon={(props) => (
+                  <MaterialCommunityIcons
+                    name="logout"
+                    size={24}
+                    color={props.color}
+                  />
+                )}
+              />
+              <Divider />
+            </View>
+          </View>
+        </DrawerContentScrollView>
+      )}
+    >
+      {children}
+    </Drawer.Navigator>
+  );
+};
+
+export default DrawerContent;
--- src/navigation/Employee/MainNavigation.js
+++ src/navigation/Employee/MainNavigation.js
@@ -1,16 +1,8 @@
 import React from "react";
-import { View, Text } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { Avatar, Divider, Caption } from "react-native-paper";
-import { useSelector } from "react-redux";
-import {
-  createDrawerNavigator,
-  DrawerContentScrollView,
-  DrawerItemList,
-  DrawerItem,
-} from "@react-navigation/drawer";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
-import Colors from "../../constants/Colors";
+import DrawerContent from "../DrawerContent";
 
 import BottomNavigation from "./BottomNavigation";
 import {
@@ -22,66 +14,8 @@ import {
 const MainNavigation = () => {
   const Drawer = createDrawerNavigator();
 
-  const firstName = useSelector((state) => state.auth.firstName);
-  const lastName = useSelector((state) => state.auth.lastName);
-
   return (
-    <Drawer.Navigator
-      drawerContentOptions={{ activeTintColor: Colors.primaryOrange }}
-      initialRouteName="HomeDrawer"
-      drawerContent={(props) => (
-        <DrawerContentScrollView contentContainerStyle={{ flex: 1 }}>
-          <View style={{ flex: 1 }}>
-            <View style={{ flexDirection: "row", alignItems: "center" }}>
-              <Avatar.Image
-                source={{
-                  uri:
-                    "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-                }}
-                style={{ margin: 15 }}
-                size={80}
-              />
-
-              <View>
-                <Text style={{ fontSize: 15, fontWeight: "bold" }}>
-                  {`${firstName} ${lastName}`}
-                </Text>
-                <View style={{ flexDirection: "row" }}>
-                  <Caption style={{ lineHeight: 14, marginRight: 3 }}>
-                    5.0
-                  </Caption>
-                  <MaterialCommunityIcons
-                    name="star"
-                    style={{ alignSelf: "center" }}
-                  />
-                </View>
-              </View>
-            </View>
-            <Divider />
-
-            <View style={{ flex: 1, marginTop: 10 }}>
-              <DrawerItemList {...props} />
-              <Divider />
-            </View>
-            <View style={{ marginVertical: 20, justifyContent: "center" }}>
-              <Divider />
-              <DrawerItem
-                label="Log Out"
-                //onPress={logoutHandler}
-                icon={(props) => (
-                  <MaterialCommunityIcons
-                    name="logout"
-                    size={24}
-                    color={props.color}
-                  />
-                )}
-              />
-              <Divider />
-            </View>
-          </View>
-        </DrawerContentScrollView>
-      )}
-    >
+    <DrawerContent>
       <Drawer.Screen
         name="HomeDrawer"
         component={BottomNavigation}
@@ -132,7 +66,7 @@ const MainNavigation = () => {
           ),
         }}
       />
-    </Drawer.Navigator>
+    </DrawerContent>
   );
 };
 
