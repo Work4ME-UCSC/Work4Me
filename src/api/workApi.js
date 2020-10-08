0feaf8f416908c3007e8ab2461bc5dd74720b87d
ChamodiKu <kulathungachamodi@gmail.com>
2020-10-08 20:44:19 +0530

    workapi

--- src/api/workApi.js
+++ src/api/workApi.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://07298e4f5986.ngrok.io", //update the link by runnig ngrok http port_no
+  baseURL: "http://c0fbfaec2e25.ngrok.io", //update the link by runnig ngrok http port_no
 });
